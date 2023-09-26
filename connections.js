6e041d6325d46b5b27a91924a1fea71b1f5c2780
Kev1nJ <kevinjerome85@yahoo.com>
2023-09-25 23:25:18 -0400

    connections visible

---
+++ connections.js
@@ -0,0 +1,34 @@
+const mysql = require('mysql');
+const fs = require("fs");
+
+// MySQL connection
+const connection = mysql.createConnection({
+  host: '127.0.0.1',
+  user: 'root',
+  password: 'LiquidSwords329388!',
+  database: 'employeecmddb',
+});
+
+// Connect to the database
+connection.connect((err) => {
+  if (err) {
+    console.error('Error connecting to MySQL: ' + err.stack);
+    return;
+  }
+  console.log('Connected to MySQL as id ' + connection.threadId);
+});
+
+module.exports = connection;
+
+// Read the schema.sql file
+const schemaSql = fs.readFileSync('db/schema.sql', 'utf8');
+
+// Execute the SQL script to initialize the database
+connection.query(schemaSql, (err) => {
+  if (err) {
+    console.error('Error initializing the database:', err);
+  } else {
+    console.log('Database initialized.');
+  }
+  connection.end();
+});
